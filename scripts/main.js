627f123ba20bcb8f3c49949fcd6bff540efb55a3
Bastien Fournier <bastien.fournier06@gmail.com>
2024-03-16 19:08:00 +0100

    ajout balise script sur page index.html et début de code sur page main.js

---
+++ scripts/main.js
@@ -0,0 +1,8 @@
+document.addEventListener("DOMContentLoaded", ready);
+
+let etatInitial = true;
+const imageBack = document.getElementById (#backImage);
+const imageFront = document.getElementById (#frontImage);
+const ballText = document.getElementById (#ballText);
+const submitButton = document.getElementById (#submitButton);
+const textArea = document.getElementById (#question);
